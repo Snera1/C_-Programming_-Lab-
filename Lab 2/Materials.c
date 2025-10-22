a81ae7e2c069366dd54c21364bac0ce54aaf66c3
Snera1 <meb25040@tezu.ac.in>
2025-10-22 22:40:37 +0530

    Update Materials.c

--- Lab 2/Materials.c
+++ Lab 2/Materials.c
@@ -1,11 +1,23 @@
-# include <stdio.h>
-int main ()
-{
-	float a, b;
-	scanf("Tensile strength of two materials: in (Mpa)");
-	printf("Tensile strength of material 1", a);
-	printf("Tensile strength of material 2", b);
-	
-	return 0;
-  
+#include <stdio.h>
+
+int main() {
+    float strength1, strength2;
+
+    // Prompt user for input
+    printf("Enter tensile strength of Material 1 (in MPa): ");
+    scanf("%f", &strength1);
+
+    printf("Enter tensile strength of Material 2 (in MPa): ");
+    scanf("%f", &strength2);
+
+    // Compare and display the larger tensile strength
+    if (strength1 > strength2) {
+        printf("The larger tensile strength is: %.2f MPa\n", strength1);
+    } else if (strength2 > strength1) {
+        printf("The larger tensile strength is: %.2f MPa\n", strength2);
+    } else {
+        printf("Both materials have equal tensile strength: %.2f MPa\n", strength1);
+    }
+
+    return 0;
 }
