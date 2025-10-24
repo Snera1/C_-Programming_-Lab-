b746ae33f17015454b5d1f5649dc4be6886f8fad
Snera1 <meb25040@tezu.ac.in>
2025-10-24 09:28:52 +0530

    Add files via upload

---
+++ Assignment/Electricity Bill.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill = 0;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 100) {
+        bill = units * 1.5;
+    } else if (units <= 200) {
+        bill = 100 * 1.5 + (units - 100) * 2;
+    } else (units <= 300) {
+        bill = 100 * 1.5 + 100 * 2 + (units - 200) * 3;
+    } else {
+        bill = 100 * 1.5 + 100 * 2 + 100 * 3 + (units - 300) * 5;
+    }
+
+    printf("Total Bill = ₹%d
+", bill);
+
+    return 0;
+}
---
+++ Assignment/Sum of first natural numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    sum = n * (n + 1) / 2;
+    printf("Sum of first %d natural numbers is: %d
+", n, sum);
+
+    return 0;
+}
