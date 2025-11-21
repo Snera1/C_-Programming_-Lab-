aab14292c96da083c22e9e562d2f8afefa526536
Snera1 <meb25040@tezu.ac.in>
2025-11-21 19:54:56 +0530

    consonant_vowels_loop.c

--- Lab_6/consonant_vowels_loop.c
+++ Lab_6/consonant_vowels_loop.c
@@ -1,32 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-	
-	char letter;
-	for(letter = 'A' ; letter<='Z' ; letter++) {
-		switch(letter) {
-			case 'A':
-				printf("%c is a vowel \n", letter);
-				break;
-			case 'E':
-				printf("%c is a vowel \n", letter);
-				break;
-			case 'I':
-				printf("%c is a vowel \n", letter);
-				break;
-			case 'O':
-				printf("%c is a vowel \n", letter);
-				break;
-			case 'U':
-				printf("%c is a vowel \n", letter);
-				break;
-			default:
-				printf("%c is a consonent \n" , letter);
-				break;
-								
-		}
-		
-	}
-	printf("above are all the alphabets");
-	return 0;
+    char letter;
+
+    for (letter = 'A'; letter <= 'Z'; letter++) {
+        switch (letter) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                printf("%c is a vowel\n", letter);
+                break;
+            default:
+                printf("%c is a consonant\n", letter);
+                break;
+        }
+    }
+
+    printf("Above are all the alphabets\n");
+    return 0;
 }
