68d61325d8dbd4c7f87e9275f4b3a138f68e5309
Snera1 <meb25040@tezu.ac.in>
2025-11-21 19:41:15 +0530

    type_find_data.c

--- Lab_5/type_find_data.c
+++ Lab_5/type_find_data.c
@@ -1,28 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-	
-	char letter;
-	int type;
-	
-	printf("Enter a letter , character, number or symbol");
-	scanf("%c" , &letter);
-	type = letter;
-	
-	if ( letter >= 65 && letter <=90) {
-		
-		printf("given input is upper case");
-	} else if(letter >= 97 && letter <=122) {
-		
-		printf("given input is lower class");
-	} else if(letter >= 48 && letter <=57) {
-		
-		printf("given input is a number");
-	} else {
-		printf("given input special symbol");
-	}
-	
-	return 0;
-	
-	
+    char letter;
+
+    printf("Enter a letter, character, number or symbol: ");
+    scanf("%c", &letter);
+
+    if (letter >= 'A' && letter <= 'Z') {
+        printf("Given input is upper case\n");
+    } else if (letter >= 'a' && letter <= 'z') {
+        printf("Given input is lower case\n");
+    } else if (letter >= '0' && letter <= '9') {
+        printf("Given input is a number\n");
+    } else {
+        printf("Given input is a special symbol\n");
+    }
+    return 0;
 }
