ba9e47ace6fa374fa02cb4678382d40c13e2c3a3
Snera1 <meb25040@tezu.ac.in>
2025-11-21 19:38:44 +0530

    floatoutput.c

--- Lab_5/floatoutput.c
+++ Lab_5/floatoutput.c
@@ -1,16 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-	
-	
-	//answer will be till 6 decimal places
-	//we will get the answer 2.50000.
-	//the answer will be same even if we switch the data types of a and b.
-	
-	int a = 5;
-	float b = 2;
-	printf("%f" , a/b);
-	
-	
-	return 0;
+    float a = 5.0, b = 2.0;
+    printf("Division is: %.2f\n", a/b);
+    return 0;
 }
