83e1cea92061dec91aa3cbbf80162825cae7c7b2
Snera1 <meb25040@tezu.ac.in>
2025-11-21 20:03:48 +0530

    while_loop_sum.c

--- Lab_6/while_loop_sum.c
+++ Lab_6/while_loop_sum.c
@@ -1,18 +1,13 @@
-#include<stdio.h>
-//first condition to see a loop runs is intialization.
-//sum of first N natural numbers.
-int main() 
-{
-	int N, count , sum ;
-	scanf("%d", &N);
-	
-	sum=0;
-	count=1;
-	while(count<=N) {
-		sum=sum+count;
-		count++;
-	}
-	
-	printf("sum=%d", sum);
-	return 0;
+#include <stdio.h>
+
+int main() {
+    int N, sum = 0;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        sum += i;
+    }
+
+    printf("sum=%d\n", sum);
+    return 0;
 }
