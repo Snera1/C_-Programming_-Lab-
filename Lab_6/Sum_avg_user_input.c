6bc2664edb2f3be783400e46e9611af711f20a5d
Snera1 <meb25040@tezu.ac.in>
2025-11-21 19:52:41 +0530

    sum_average_user_input.c

--- Lab_6/Sum_avg_user_input.c
+++ Lab_6/Sum_avg_user_input.c
@@ -1,22 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-	
-	int n;
-	int j;
-	int i=1;
-	float sum=0;
-	printf("Enter how many entries you want \n");
-	scanf("%d" , &n);
-	while(i<=n) {
-		printf("enter your number");
-		scanf("%d" , &j);
-		i++;
-		sum=sum+j;
-	}
-	printf("sum of all numbers = %.2f \n" , sum );
-	printf("average of given numbers = %.2f" , sum/n);
-	
-	return 0;
-	
+    int n;
+    float sum = 0;
+
+    printf("Enter how many entries you want: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int t;
+        printf("Enter your number: ");
+        scanf("%d", &t);
+        sum += t;
+    }
+
+    printf("Sum of all numbers = %.2f\n", sum);
+    printf("Average of given numbers = %.2f\n", sum / n);
+
+    return 0;
 }
