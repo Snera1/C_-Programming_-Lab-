46c4077f5b4e399d8dd6e729d327080e027874a5
Snera1 <meb25040@tezu.ac.in>
2025-11-21 19:58:02 +0530

    even_&_odd_num.c

--- Lab_6/even_num.c
+++ Lab_6/even_num.c
@@ -1,21 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-	
-	int n=100;
-	int i=1;
-	int first = 2;
-	
-	while(first<=99) {
-		if(first % 2==0) {
-			printf("even number = %d \n"  , first);
-		}
-		first++;
-	}
-	printf("above are all the even nos btw 1 and 100");
-	return 0;
-	
-	
+    for (int i = 2; i <= 100; i += 2) {
+        printf("Even number = %d\n", i);
+    }
+    printf("Above are all the even numbers between 1 and 100\n");
+    return 0;
 }
+
 
 
